4f49375091b9de0f95559527dade95d08a575549
gyeom-yee <dbsrua1105@naver.com>
2023-11-17 12:47:02 +0900

    feat: Print fizzbuzz if i is multiple of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 # i -> print("Hello") -> esc -> :wq +enter
 
-for i in range(1, 12+1):
-    if i%3==0:
+for i in range(1, 16):
+    if i%(3*5)==0:
+        print('fizzbuzz')
+    elif i%3==0:
         print("fizz")
     elif i%5==0:
         print('buzz')
